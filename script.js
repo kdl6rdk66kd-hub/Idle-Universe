c8f5117983d92e064b46d048c065374ae6b4946c
kdl6rdk66kd-hub <kevinsarracina9@gmail.com>
2026-07-01 18:00:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let coins = 0;
+let cps = 0;
 
+const core = document.getElementById("core");
+
+function update(){
+  document.getElementById("coins").innerText = Math.floor(coins);
+  document.getElementById("cps").innerText = cps.toFixed(1);
+}
+
+core.onclick = () => {
+  coins += 1;
+  update();
+};
+
+// auto income base
+setInterval(() => {
+  coins += cps / 10;
+  update();
+}, 100);
+
+// upgrade de prueba (para empezar)
+setTimeout(() => {
+  cps = 1;
+}, 3000);
+
+update();
